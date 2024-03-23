f5f7ef24c1f9896c49819e5a6c4e842e23ac5fe8
Vedad <vedad.keskin98@gmail.com>
2024-03-23 01:45:33 +0100

    Odrada drugog ispitnog u februaru 2024

---
+++ Workshops/ISPITNI FEB-22-2024/Template 2024/FIT.WinForms/IspitIB180079/frmPrisustvoIB180079.cs
@@ -0,0 +1,175 @@
+﻿using FIT.Data;
+using FIT.Data.IspitIB180079;
+using FIT.Infrastructure;
+using FIT.WinForms.Helpers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIT.WinForms.IspitIB180079
+{
+    public partial class frmPrisustvoIB180079 : Form
+    {
+        private ProstorijeIB180079 odabranaProstorija;
+        DLWMSDbContext db = new DLWMSDbContext();
+        int brojStudenataNaNastavi = 0;
+        List<PrisustvoIB180079> prisustva;
+        NastavaIB180079 odabranaNastava;
+
+        public frmPrisustvoIB180079(ProstorijeIB180079 odabranaProstorija)
+        {
+            InitializeComponent();
+            this.odabranaProstorija = odabranaProstorija;
+        }
+
+        private void frmPrisustvoIB180079_Load(object sender, EventArgs e)
+        {
+            dgvNastava.AutoGenerateColumns = false;
+            lblProstorija.Text = $"{odabranaProstorija.Naziv} - {odabranaProstorija.Oznaka}";
+            cbNastava.DataSource = db.NastavaIB180079.Include(x => x.Predmet).Where(x => x.ProstorijaId == odabranaProstorija.Id).ToList();
+            cbStudent.DataSource = db.Studenti.ToList();
+            UcitajPrisustvaNastave();
+            UcitajPrebrojavanje();
+        }
+
+        private void UcitajPrisustvaNastave()
+        {
+            odabranaNastava = cbNastava.SelectedItem as NastavaIB180079;
+
+
+
+            prisustva = db.PrisustvoIB180079.Include(x => x.Student).Include(x => x.Nastava).ThenInclude(x => x.Predmet).Where(x => x.NastavaId == odabranaNastava.Id).ToList();
+
+            if (prisustva != null)
+            {
+                for (int i = 0; i < prisustva.Count(); i++)
+                {
+                    prisustva[i].Info = $"{prisustva[i].Nastava.Predmet} - u {prisustva[i].Nastava.Dan} @ {prisustva[i].Nastava.Vrijeme}";
+                }
+
+                brojStudenataNaNastavi = prisustva.Count();
+
+                dgvNastava.DataSource = null;
+                dgvNastava.DataSource = prisustva;
+            }
+            UcitajPrebrojavanje();
+        }
+
+        private void UcitajPrebrojavanje()
+        {
+            lblOdDo.Text = $"{brojStudenataNaNastavi}/{odabranaProstorija.Kapacitet}";
+        }
+
+        private void cbNastava_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UcitajPrisustvaNastave();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var student = cbStudent.SelectedItem as Student;
+            var nastava = cbNastava.SelectedItem as NastavaIB180079;
+
+            var novoPrisustvo = new PrisustvoIB180079()
+            {
+                StudentId = student.Id,
+                NastavaId = nastava.Id
+
+            };
+
+            if (db.PrisustvoIB180079.ToList().Exists(x => x.StudentId == student.Id && x.NastavaId == nastava.Id))
+            {
+                MessageBox.Show("Student je već evidentiran na toj nastavi", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }else if (odabranaProstorija.Kapacitet == brojStudenataNaNastavi)
+            {
+                MessageBox.Show("Dosegli ste maksimalan broj studenata u prostoriji", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                db.PrisustvoIB180079.Add(novoPrisustvo);
+                db.SaveChanges();
+
+                MessageBox.Show("Prisustvo je evidentirano", "Informacija", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+
+
+            UcitajPrisustvaNastave();
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (Validiraj())
+            {
+                var odabraniStudent = cbStudent.SelectedItem as Student;
+
+                Thread thread = new Thread(() => GenerisiPrisustva(odabraniStudent));
+                thread.Start();
+            }
+        }
+
+        private void GenerisiPrisustva(Student? odabraniStudent)
+        {
+            Thread.Sleep(300);
+
+            var broj = int.Parse(txtBroj.Text);
+            var info = "";
+            var sveNastaveProstorije = db.NastavaIB180079.Where(x => x.ProstorijaId == odabranaProstorija.Id).ToList();
+
+            if(brojStudenataNaNastavi + broj >= odabranaProstorija.Kapacitet)
+            {
+                MessageBox.Show("Ako generišete ta prisustva prekoračit ćete maksimalan kapacitet prostorije na nekoj nastavi", "Upozorenje", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+             for (int i = 0; i < broj; i++)
+             {
+
+                 for (int j = 0; j < sveNastaveProstorije.Count(); j++)
+                 {
+
+                     var novoPrisustvo = new PrisustvoIB180079()
+                     {
+                         StudentId = odabraniStudent.Id,
+                         NastavaId = sveNastaveProstorije[j].Id
+                     };
+
+                     db.PrisustvoIB180079.Add(novoPrisustvo);
+
+                     info += $"{DateTime.Now.ToString("dd.MM HH:mm:ss")} -> {odabraniStudent} za  {sveNastaveProstorije[j].Predmet} u {sveNastaveProstorije[j].Dan} @{sveNastaveProstorije  [j].Vrijeme} {Environment.NewLine}";
+
+                 }
+
+
+
+           }
+
+
+
+            db.SaveChanges();
+
+            Action action = () =>
+            {
+                UcitajPrisustvaNastave();
+                MessageBox.Show($"Prisustva su generisana", "Informacija", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtInfo.Text = info;
+
+            };
+            BeginInvoke(action);
+        }
+
+        private bool Validiraj()
+        {
+            return Validator.ProvjeriUnos(txtBroj, err, Kljucevi.ReqiredValue);
+        }
+    }
+}
---
+++ Workshops/ISPITNI FEB-22-2024/Template 2024/FIT.WinForms/IspitIB180079/frmProstorijeIB180079.cs
@@ -0,0 +1,123 @@
+﻿using FIT.Data.IspitIB180079;
+using FIT.Infrastructure;
+using FIT.WinForms.Izvjestaji;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIT.WinForms.IspitIB180079
+{
+    public partial class frmProstorijeIB180079 : Form
+    {
+        DLWMSDbContext db = new DLWMSDbContext();
+        List<ProstorijeIB180079> prostorije;
+        ProstorijeIB180079 odabranaProstorija;
+        public frmProstorijeIB180079()
+        {
+            InitializeComponent();
+        }
+
+        private void frmProstorijeIB180079_Load(object sender, EventArgs e)
+        {
+            dgvProstorije.AutoGenerateColumns = false;
+            UcitajProstorije();
+        }
+
+        private void UcitajProstorije()
+        {
+
+            prostorije = db.ProstorijeIB180079.ToList();
+
+
+            if (prostorije != null)
+            {
+
+                for (int i = 0; i < prostorije.Count(); i++)
+                {
+                    var sveNastave = db.NastavaIB180079.ToList();
+
+                    prostorije[i].Broj = sveNastave.Where(x => x.ProstorijaId == prostorije[i].Id).Count();
+                }
+
+
+                dgvProstorije.DataSource = null;
+                dgvProstorije.DataSource = prostorije;
+            }
+        }
+
+        private void btnPrintaj_Click(object sender, EventArgs e)
+        {
+            odabranaProstorija = dgvProstorije.SelectedRows[0].DataBoundItem as ProstorijeIB180079;
+
+            frmIzvjestaji frmIzvjestaj = new frmIzvjestaji(odabranaProstorija);
+            frmIzvjestaj.ShowDialog();
+        }
+
+        private void btnNova_Click(object sender, EventArgs e)
+        {
+            frmNovaProstorijaIB180079 frmNovaProstorija = new frmNovaProstorijaIB180079();
+            if (frmNovaProstorija.ShowDialog() == DialogResult.OK)
+            {
+                UcitajProstorije();
+                MessageBox.Show("Zapis je dodan", "Informacija", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void dgvProstorije_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            odabranaProstorija = prostorije[e.RowIndex];
+
+            if (e.ColumnIndex == 5 || e.ColumnIndex == 6) // double click na dva buttona koje necemo koristit jer nam treba jednostruki klik na njih
+            {
+
+            }
+            else
+            {
+                frmNovaProstorijaIB180079 frmNovaProstorija = new frmNovaProstorijaIB180079(odabranaProstorija);
+                if (frmNovaProstorija.ShowDialog() == DialogResult.OK)
+                {
+                    UcitajProstorije();
+                    MessageBox.Show("Zapis je modifikovan", "Informacija", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void dgvProstorije_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            odabranaProstorija = prostorije[e.RowIndex];
+
+            if (e.ColumnIndex == 5) // pregled nastave
+            {
+                frmNastavaIB180079 frmNastava = new frmNastavaIB180079(odabranaProstorija);
+                if(frmNastava.ShowDialog() == DialogResult.OK)
+                {
+                    UcitajProstorije();
+                }
+            }
+            else if (e.ColumnIndex == 6) // pregled prisustva
+            {
+                var brojNastavaUProstoriji = db.NastavaIB180079.Where(x => x.ProstorijaId == odabranaProstorija.Id).Count();
+
+                if(brojNastavaUProstoriji != 0)
+                {
+                    frmPrisustvoIB180079 frmPrisustvo = new frmPrisustvoIB180079(odabranaProstorija);
+                    frmPrisustvo.ShowDialog();
+
+                }
+                else
+                {
+                    MessageBox.Show("Prostorija nema zakazanih nastava", "Upozorenje", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+
+        }
+    }
+}
